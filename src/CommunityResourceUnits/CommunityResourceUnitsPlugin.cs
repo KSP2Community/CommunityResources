878f764cef2133aea61bb300741fd3a8d8fcf568
Lexi <arallen3660@gmail.com>
2023-09-01 20:06:32 -0400

    Add ability to define custom resource units

---
+++ src/CommunityResourceUnits/CommunityResourceUnitsPlugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using JetBrains.Annotations;
+using KSP.Game;
+using Newtonsoft.Json.Linq;
+using SpaceWarp;
+using SpaceWarp.API.Mods;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace CommunityResourceUnits;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
+public class CommunityResourceUnitsPlugin : BaseSpaceWarpPlugin
+{
+    // These are useful in case some other mod wants to add a dependency to this one
+    [PublicAPI] public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
+    [PublicAPI] public const string ModName = MyPluginInfo.PLUGIN_NAME;
+    [PublicAPI] public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
+
+    // Singleton instance of the plugin class
+    public static CommunityResourceUnitsPlugin Instance { get; set; }
+
+    /// <summary>
+    /// Runs when the mod is first initialized.
+    /// </summary>
+    public override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Instance = this;
+        GameManager.Instance.Assets.LoadByLabel<TextAsset>("resource_units",RegisterUnits,delegate(IList<TextAsset> assetLocations)
+        {
+            if (assetLocations != null)
+            {
+                Addressables.Release(assetLocations);
+            }
+        });
+    }
+
+    private static void RegisterUnits(TextAsset asset)
+    {
+        var dict = JObject.Parse(asset.text);
+        foreach (var value in dict)
+        {
+            Patches.ResourceUnits[value.Key] = value.Value.Value<string>();
+        }
+    }
+}
---
+++ src/CommunityResourceUnits/Patches.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using KSP.Game;
+using KSP.UI;
+
+namespace CommunityResourceUnits;
+
+internal static class Patches
+{
+    internal static readonly Dictionary<string, string> ResourceUnits = new Dictionary<string, string>();
+
+    [HarmonyPatch(typeof(ResourceInfoDataContext), nameof(ResourceInfoDataContext.GetUnits))]
+    [HarmonyPrefix]
+    private static bool ResourceInfoDataContext_GetUnits(string resourceName, ref string __result)
+    {
+        if (ResourceUnits.TryGetValue(resourceName, out __result))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    [HarmonyPatch(typeof(ResourceManagerContainerEntry), nameof(ResourceManagerContainerEntry.GetUnits))]
+    [HarmonyPrefix]
+    private static bool ResourceManagerContainerEntry_GetUnits(string resourceName, ref string __result)
+    {
+        if (ResourceUnits.TryGetValue(resourceName, out __result))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
